1aa1f1cad16adeed2e0f6b776160ce1a4c42c0aa
hongge <1012090873@qq.com>
2015-08-12 22:02:05 +0800

    delete update

--- app/src/main/java/com/cxwl/agriculture/fragement/AboutFragment.java
+++ app/src/main/java/com/cxwl/agriculture/fragement/AboutFragment.java
@@ -8,11 +8,8 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
-import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -36,11 +33,6 @@ import com.cxwl.agriculture.netInterface.NetUtil;
 import org.json.JSONObject;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.RandomAccessFile;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -58,56 +50,11 @@ public class AboutFragment extends Fragment implements View.OnClickListener{
     private TextView introduction;
     private TextView checkupdate;
     private ProgressDialog progressDialog;
-    private int retry;
-    private int TryDownTotal=2;//下载失败再次尝试的次数
-    private ProgressDialog pbar;
-    private String fileName = "agriculture.apk";
-    private String path;
-    private int fileTotalSize = 0;
-    private Handler mHandler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what){
-                case 3:
-                    ReplaceLaunchApk(path+"/"+fileName);
-                    break;
-                case 4:
-                    new AlertDialog.Builder(getActivity())
-                            .setTitle("错误")
-                            .setMessage("请插入外部存储卡")
-                            .setCancelable(false)
-                            .setNegativeButton("确定", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    dialogInterface.dismiss();
-                                }
-                            })
-                            .show();
-                    break;
-                case 5:
-                    new AlertDialog.Builder(getActivity())
-                            .setTitle("错误")
-                            .setMessage("存储卡剩余的空间不足")
-                            .setCancelable(false)
-                            .setNegativeButton("确定", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    dialogInterface.dismiss();
-                                }
-                            })
-                            .show();
-
-                    break;
-                default:
-                    break;
-            }
-        }
-    };
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.i("test", "onCreateView======"+this.getClass().getSimpleName());
+        Log.i("test", "onCreateView======" + this.getClass().getSimpleName());
         if(rootView == null){
             rootView = inflater.inflate(R.layout.fragment_aboutus, null);
             init();
@@ -207,16 +154,6 @@ public class AboutFragment extends Fragment implements View.OnClickListener{
         });
     }
 
-    private void getTotalSize(String url){
-        try {
-            URL u = new URL(url);
-            HttpURLConnection urlcon = (HttpURLConnection) u.openConnection();
-            fileTotalSize =  urlcon.getContentLength();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
 
 
     private void showUpdateDialog(String message, final String url){
@@ -228,7 +165,6 @@ public class AboutFragment extends Fragment implements View.OnClickListener{
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         dialogInterface.dismiss();
-                        predownloadapk(url);
                     }
                 })
                 .setNegativeButton("以后再说", new DialogInterface.OnClickListener() {
@@ -269,164 +205,6 @@ public class AboutFragment extends Fragment implements View.OnClickListener{
         }
     }
 
-    private void predownloadapk(String apkDownUrl)
-    {
-        retry=0;
-        getTotalSize(apkDownUrl);
-        //创建文件
-        downloadapk(apkDownUrl);
-    }
-
-    private void downloadapk(String apkDownUrl)
-    {
-        //
-        pbar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        pbar.setMessage("正在下载中，请稍后...");
-        pbar.setCancelable(false);
-        pbar.setMax(100);
-        pbar.setProgress(0);
-        pbar.show();
-
-        new DownloadFilesTask().execute(apkDownUrl);
-
-    }
-
-    class DownloadFilesTask extends AsyncTask<String, Integer, Long> {
-
-        private URL url; // 资源位置
-        private long beginPosition; // 开始位置
-
-
-        @Override
-        protected Long doInBackground(String... params) {
-
-
-            InputStream inputStream = null;
-            HttpURLConnection httpURLConnection = null;
-            RandomAccessFile output = null;
-            try {
-                // 获取连接对象h
-
-
-                //创建包名的文件夹
-                boolean hasSD = Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);          //只检验是否可读且可
-                if (!hasSD) {
-
-                    Message msg = Message.obtain(mHandler);
-                    msg.what = 4;
-                    mHandler.sendMessage(msg);
-                    return (long) 0;
-                }
-                long availablesize = getUsableStorage();
-                if (availablesize < 10) {
-
-                    Message msg = Message.obtain(mHandler);
-                    msg.what = 5;
-                    mHandler.sendMessage(msg);
-                    return (long) 0;
-                }
-
-                String sddir = getSDCardPath();
-                String packageName = getActivity().getPackageName();
-
-                path = sddir + "/" + packageName;
-                File destDir = new File(sddir + "/" + packageName);
-                if (!destDir.exists()) {
-                    destDir.mkdirs();
-                }
-
-                File outFile = new File(sddir + "/" + packageName + "/" + fileName);
-                if (!outFile.exists()) {
-                    try {
-                        //在指定的文件夹中创建文件
-                        outFile.createNewFile();
-                    } catch (Exception e) {
-                    }
-                }
-
-                // 通过文件创建输出流对象RandomAccessFile,r:读 w:写 d:删除
-                output = new RandomAccessFile(outFile, "rw");
-                // 设置写入位置
-
-                long lenth = outFile.length();
-
-
-                if (fileTotalSize == lenth) {
-
-                    //下载完的处理
-                    publishProgress(100);
-                    Message msg = Message.obtain(mHandler);
-                    msg.what = 3;
-                    mHandler.sendMessage(msg);
-                    return lenth;
-
-                } else {
-                    beginPosition = lenth;
-                }
-
-                // 设置断点续传的开始位置
-                url = new URL(params[0]);
-                httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setAllowUserInteraction(true);
-                httpURLConnection.setRequestMethod("GET");
-                httpURLConnection.setReadTimeout(4000);
-                // httpURLConnection.setRequestProperty("User-Agent","NetFox");
-                httpURLConnection.setRequestProperty("Range", "bytes=" + beginPosition + "-");
-                inputStream = httpURLConnection.getInputStream();
-
-                output.seek(beginPosition);
-                byte[] buf = new byte[1024 * 100];
-                int readsize = 0;
-                // 进行循环输出
-                while ((readsize = inputStream.read(buf)) != -1) {
-                    output.write(buf, 0, readsize);
-                    beginPosition += readsize;
-                    publishProgress((int) (beginPosition * 100.0f / fileTotalSize));
-                }
-
-
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            } finally {
-                if (inputStream != null) {
-                    try {
-                        inputStream.close();
-                        if (output != null) {
-                            output.close();
-                        }
-                        if (httpURLConnection != null) {
-                            httpURLConnection.disconnect();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            return beginPosition;
-        }
-
-        /**
-         * 更新下载进度，当publishProgress方法被调用的时候就会自动来调用这个方法
-         */
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
-
-            pbar.setProgress(values[0]);
-            if (values[0] == 100) {
-                Message msg = Message.obtain(mHandler);
-                msg.what = 3;
-                mHandler.sendMessage(msg);
-            }
-        }
-
-        //下载完的回调
-        @Override
-        protected void onPostExecute(Long size) {
-
-        }
-    }
 
     /**
      * 获取外置SD卡路径
